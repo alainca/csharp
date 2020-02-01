0b524673c851886cea41d8fe2a72895b9f4c49e1
alainca <36841974+alainca@users.noreply.github.com>
2020-02-01 22:33:05 +0100

    LIgnes du fichier texte en tableau

---
+++ fichier txt en tableau/ClassLibrary1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    namespace classereleve
+    {
+        public class Releve
+        {
+            public string Number;
+            public string Date;
+            public string Heure;
+            public string Temp;
+            public string Hum;
+
+        }
+        public class ListRelev
+        {
+
+        }
+    }
+}
---
+++ fichier txt en tableau/fichier txt en tableau/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using ClassLibrary1.classereleve;
+
+namespace fichier_txt_en_tableau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StreamReader reader = new StreamReader("D:/cesi/c sharp/projet4/releve.txt");
+
+            string ligne;
+
+            ligne = reader.ReadLine();
+            for (int i = 0; ligne != null; i++)  // Pour chaque ligne
+            {
+                string[] sousTab = ligne.Split(' '); // On récupère les 4 éléments dans un tableau
+                Releve rel = new Releve();
+                rel.Number = sousTab[0];
+                rel.Date = sousTab[1];
+                rel.Heure = sousTab[2];
+                rel.Temp = sousTab[3];
+                rel.Hum = sousTab[4];
+                List<Releve> releve = new List<Releve>();
+                releve.Add(rel);
+                ligne = reader.ReadLine(); // On passe à la ligne suivante
+
+               
+
+                /* public int Number
+                        {
+                            get { return _number; }
+                            set { _number = value; }
+                        }
+                        */
+
+
+
+                System.Console.WriteLine("Numero : " + rel.Number + ", Date : " + rel.Date + ", L'Heure : " + rel.Heure + ", La température : " + rel.Temp + "L'humidité : " + rel.Hum);
+
+            }
+            {
+                Console.ReadLine();
+            }
+        }
+
+
+    }
+}
+   
