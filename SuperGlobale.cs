5bb2aced043b85869294e4c9b35837a27522dd0f
alainca <36841974+alainca@users.noreply.github.com>
2020-02-12 06:12:28 +0100

    Super globale

---
+++ SuperGlobale.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libraryreleve.Service
+{
+    public class superglobale
+    {
+        //global PathFile
+        private static string v_GlobalPath = "";
+        public static string GlobalPath
+        {
+            get { return v_GlobalPath; }
+            set { v_GlobalPath = value; }
+        }
+    }
+}
